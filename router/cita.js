2a2ebb720443ba65ddf61e54f9c28f24b8b32834
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-22 17:25:56 -0500

    feat: :poop: Metodo get  para la proxima cita
    Se realizo el metodo get de la proxima cita pero le faltan cosas para que funcione correctamente

--- router/cita.js
+++ router/cita.js
@@ -16,4 +16,15 @@ appCita.get("/", async(req, res)=>{
 
 });
 
+appCita.get("/proxima-cita/:usu_id", async(req, res)=>{
+
+    const usu_id = req.params.usu_id;
+
+    let db = await conexion();
+    let Cita = db.collection('cita');
+    let result = await Cita.findOne({ cit_datosUsuario: usu_id, cit_estadoCita: { est_cita_id: 1, est_cita_nombre: "Pendiente" } },
+    { sort: { cit_fecha: 1 } });
+    res.send(result);
+})
+
 export default appCita;
