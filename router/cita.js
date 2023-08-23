206a6be686b2389778adbc2a2f4ed415fe1f85c8
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-22 22:29:35 -0500

    feat: :sparkles: metodo get terminado
    Se completo el metodo get el cual trae la proxiam cita de un paciente

--- router/cita.js
+++ router/cita.js
@@ -16,15 +16,31 @@ appCita.get("/", async(req, res)=>{
 
 });
 
-appCita.get("/proxima-cita/:usu_id", async(req, res)=>{
+appCita.get("/:usu_id", async (req, res) => {
 
-    const usu_id = req.params.usu_id;
+    const usu_id = parseInt(req.params.usu_id); 
 
-    let db = await conexion();
-    let Cita = db.collection('cita');
-    let result = await Cita.findOne({ cit_datosUsuario: usu_id, cit_estadoCita: { est_cita_id: 1, est_cita_nombre: "Pendiente" } },
-    { sort: { cit_fecha: 1 } });
-    res.send(result);
-})
+    try {
+        let db = await conexion();
+        let Cita = db.collection('cita');
+        
+        let now = new Date();
+        let result = await Cita.find({
+            "cit_datosUsuario": usu_id,
+            "cit_fecha": { $gte: now }
+        }).sort({ "cit_fecha": 1 }).limit(1).toArray();
+
+        if (result.length > 0) {
+            res.send(result[0]);
+        } else {
+            res.send("No se encontraron citas futuras para el paciente.");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Ocurrió un error al obtener la próxima cita.");
+    }
+
+});
+
 
 export default appCita;
