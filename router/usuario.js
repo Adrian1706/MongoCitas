881f82daff38908ec9cb26e7512c71685b1539b1
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-22 22:49:43 -0500

    feat: :sparkles: Metodo get de usuario
    se realizo el metodo get el cual  encuentra todos los pacientes que tienen una cita con un médico en específico

--- router/usuario.js
+++ router/usuario.js
@@ -16,4 +16,25 @@ appUsuario.get("/", async(req, res)=>{
 
 });
 
+appUsuario.get("/:med_nroMatriculaProfesional", async (req, res) => {
+    const med_nroMatriculaProfesional = parseInt(req.params.med_nroMatriculaProfesional);
+    const Cita = db.collection('cita');
+    const Usuario = db.collection('usuario');
+    const Medico = db.collection('medico');
+    const medico = await Medico.findOne({
+        "med_nroMatriculaProfesional": med_nroMatriculaProfesional
+    });
+    if (!medico) {
+        return res.status(404).send("Médico no encontrado.");
+    }
+    const citasConMedico = await Cita.find({
+        "cit_medico": med_nroMatriculaProfesional
+    }).toArray();
+    const pacienteIds = citasConMedico.map(cita => cita.cit_datosUsuario);
+    const pacientes = await Usuario.find({
+        "usu_id": { $in: pacienteIds }
+    }).toArray();
+    res.send(pacientes);
+});
+
 export default appUsuario;
