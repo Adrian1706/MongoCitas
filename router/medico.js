ad47dcebf41d28da7583d5c2e79612b1e5030d7d
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-22 23:27:26 -0500

    feat: :sparkles: Metodo get medico.js
    Se realizo un metodo get en el cual se traen los datos nombre del medico y nombre del consultorio

--- router/medico.js
+++ router/medico.js
@@ -7,7 +7,7 @@ let db = await conexion();
 
 let Medico = db.collection('medico');
 
-appMedico.get("/:esp_nombre", async(req, res)=>{
+appMedico.get("/especialidad/:esp_nombre", async(req, res)=>{
 
     const esp_nombre = req.params.esp_nombre;
 
@@ -18,4 +18,20 @@ appMedico.get("/:esp_nombre", async(req, res)=>{
 
 });
 
+appMedico.get("/consultorios", async (req, res) => {
+
+    let db = await conexion();
+      let medico = db.collection("medico");
+  
+      let result = await medico
+        .find()
+        .project({
+          _id: 0,
+          med_nombreCompleto: 1,
+          "med_consultorio.cons_nombre": 1,
+        })
+        .toArray();
+      res.send(result);
+      
+});
 export default appMedico;
